e47c452460d5f5f015acca122510ba5e57d96ef4
henriquezjassis <henriquezjassis@gmail.com>
2021-06-26 17:45:00 -0300

    Adicionado Minio com sucesso

--- api/routes/api.php
+++ api/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Resources\UserResource;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Storage;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,8 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return UserResource::make($request->user());
 });
 
-// Route::group(['middleware' => 'auth:sanctum'], function(){
-Route::group([], function(){
+Route::get('teste', function(){
+    return Storage::allFiles();
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::group(['prefix' => '/users'], function() {
         Route::get('/', [UserController::class, 'index'])->middleware('can:viewAll,App\\Models\\User');
         Route::get('/{user}', [UserController::class, 'show'])->middleware('can:view,user');
